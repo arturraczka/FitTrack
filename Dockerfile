437da53bc6dc194173c93aad7d18f6e4e0e4810f
Artur Rączka <art.raczka@gmail.com>
2023-05-11 15:07:08 +0200

    - implemented postgreSQL - created two docker containers: 1 for backend, 1 for db - created 2 abstract models: Session and Summary - created 2x5 models inheriting from Session and Summary for every sport activity chosen by the team

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.11.3
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONBUFFERED 1
+ENV DEBIAN_FRONTEND=noninteractive
+WORKDIR /app
+
+COPY Pipfile Pipfile.lock /app/
+RUN pip install --upgrade pip setuptools pipenv
+RUN pipenv install --deploy --system --dev
+COPY . /app
+WORKDIR /app/fit_tracker
+CMD python manage.py runserver 0.0.0.0:8000
+
---
+++ fit_tracker/apps/api/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from apps.api.models import *
+
+# Register your models here.
+
+admin.site.register(RunningSession)
+
+admin.site.register(SwimmingSession)
+
+admin.site.register(CyclingSession)
+
+admin.site.register(WalkingSession)
+
+admin.site.register(HikingSession)
+
+admin.site.register(RunningSummary)
+
+admin.site.register(SwimmingSummary)
+
+admin.site.register(CyclingSummary)
+
+admin.site.register(WalkingSummary)
+
+admin.site.register(HikingSummary)
