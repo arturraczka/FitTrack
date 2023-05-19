afdd4c0ab94decdfba95b2b41d85f38716060df9
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-05-19 11:55:49 +0100

    Import postServices from workoutSlice

--- fit_tracker/frontend/src/components/WorkoutForm.js
+++ fit_tracker/frontend/src/components/WorkoutForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-multi-date-picker';
 import InputIcon from 'react-multi-date-picker/components/input_icon';
-import { postServices } from '../../redux/reservationSlice';
+import { postServices } from '../redux/reducers/workoutSlice';
 
 const WorkoutForm = () => {
   const [workout, setWorkout] = useState({ exercise: '', duration: 0 });
