c42a49242e2ce2c5405945d03fd6a60f40c3f80d
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-05-21 22:06:13 +0100

    Update main.js

--- fit_tracker/frontend/src/main.jsx
+++ fit_tracker/frontend/src/main.jsx
@@ -1,12 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from '../redux/configureStore';
+import store from '../src/redux/configureStore';
 import App from './App.jsx';
 import './index.css';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
 );
