f97f546da1267ab1a2acf9707a57a73ce23e3137
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-05-21 22:06:00 +0100

    Add defaultWorkouts to App

--- fit_tracker/frontend/src/App.jsx
+++ fit_tracker/frontend/src/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import WorkoutList from './WorkoutList';
+import WorkoutList from '../src/components/WorkoutList';
 
 const App = () => {
-  const workouts = useSelector((state) => state.workouts);
+  const workouts = useSelector((state) => state.formData);
+  console.log(workouts);
+
+  const defaultWorkouts = [
+    { id: 1, name: 'Pushups', duration: 10 },
+    { id: 2, name: 'Squats', duration: 15 },
+    { id: 3, name: 'Lunges', duration: 20 },
+  ];
+
+  const workoutList = workouts.length > 0 ? workouts : defaultWorkouts;
 
   return (
     <div>
       <h1>My Workouts</h1>
-      <WorkoutList workouts={workouts} />
+      <WorkoutList workouts={workoutList} />
     </div>
   );
 };
