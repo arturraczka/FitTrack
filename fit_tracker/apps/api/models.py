3d964cd09b5a8d53d248c984717b2aa59b0d1d73
Artur Raczka <art.raczka@gmail.com>
2023-05-22 12:29:37 +0200

    - created a signal for creating all 5 instances of summaries when a new user is created - created a signal for updating the summary instance whenever a user posts a session

--- fit_tracker/apps/api/models.py
+++ fit_tracker/apps/api/models.py
@@ -1,5 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.db import models
-from apps.user.models import CustomUser
+
+
+class Summary(models.Model):
+    SUMMARY_TYPE_CHOICES = [
+        ("running" , "running") ,
+        ("cycling" , "cycling") ,
+        ("hiking" , "hiking") ,
+        ("swimming" , "swimming") ,
+        ("walking" , "walking") ,
+    ]
+    user = models.ForeignKey(to=get_user_model(), on_delete = models.CASCADE)
+    summary_type = models.CharField(max_length = 10, choices = SUMMARY_TYPE_CHOICES)
+    total_distance = models.DecimalField(max_digits = 10, decimal_places = 1, null=True)
+    total_number_sessions = models.IntegerField(null=True)
+    total_length_time = models.DurationField(null=True)
+    last_session = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return (
+            f"{self.user.username}: "
+            f"{self.summary_type}"
+        )
 
 
 class Session(models.Model):
@@ -7,7 +29,7 @@ class Session(models.Model):
         ("easy", "easy"),
         ("medium", "medium"),
         ("hard", "hard"),
-]
+    ]
 
     SESSION_TYPE_CHOICES = [
         ("running", "running"),
@@ -15,13 +37,14 @@ class Session(models.Model):
         ("hiking", "hiking"),
         ("swimming" , "swimming") ,
         ("walking" , "walking") ,
-]
-    user = models.ForeignKey(to = get_user_model(), on_delete = models.CASCADE)
-    session_type = models.CharField(blank = True, max_length = 10, choices = SESSION_TYPE_CHOICES)
-    distance = models.DecimalField(max_digits = 4, decimal_places = 1, blank = False)
-    intensity = models.CharField(blank = False, max_length = 10, choices = INTENSITY_CHOICES)
-    length_time = models.DurationField(blank = False)
-    start_date = models.DateTimeField(blank = False)
+    ]
+    user = models.ForeignKey(to=get_user_model(), on_delete = models.CASCADE)
+    summary = models.ForeignKey(to=Summary, on_delete = models.CASCADE, null = True)
+    session_type = models.CharField(max_length = 10, choices = SESSION_TYPE_CHOICES)
+    distance = models.DecimalField(max_digits = 4, decimal_places = 1)
+    intensity = models.CharField(max_length = 10, choices = INTENSITY_CHOICES)
+    length_time = models.DurationField()
+    start_date = models.DateTimeField()
 
     def __str__(self):
         return (
