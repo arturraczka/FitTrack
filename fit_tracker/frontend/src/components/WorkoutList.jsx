7a79429d7e5706ef08a4adcaf710a0d91ba74761
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-05-25 05:35:00 +0100

    Use Heart Animation

--- fit_tracker/frontend/src/components/WorkoutList.jsx
+++ fit_tracker/frontend/src/components/WorkoutList.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import CountUp from 'react-countup';
+import HeartAnimation from './HeartAnimation';
+
 
 const WorkoutList = ({ workouts }) => {
+  const totalSteps = workouts.reduce((acc, workout) => acc + workout.steps, 0);
+  const totalActiveTime = workouts.reduce((acc, workout) => acc + workout.activeTime, 0);
+  const totalCaloriesBurnt = workouts.reduce((acc, workout) => acc + workout.caloriesBurnt, 0);
+
   return (
-    <div className="bg-gray-100 h-screen">
-      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <div className="px-4 py-6 sm:px-0">
-          <h1 className="text-2xl font-bold mb-4">Workout List</h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-            {workouts.map((workout) => (
-              <div
-                key={workout.id}
-                className="bg-white shadow overflow-hidden sm:rounded-lg p-4"
-              >
-                <div className="flex items-center justify-between mb-2">
-                  <h2 className="text-lg font-bold">{workout.name}</h2>
-                  <span className="text-gray-500">{workout.duration} min</span>
-                </div>
-                <p className="text-gray-500">{workout.description}</p>
-              </div>
-            ))}
+    <div className="container mx-auto px-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {workouts.map((workout) => (
+          <div key={workout.id} className="bg-white rounded-lg shadow-lg overflow-hidden">
+            <div className="p-4">
+              <h2 className="text-lg font-medium text-gray-800">{workout.name}</h2>
+              <p className="text-sm text-gray-500">{workout.date}</p>
+              <p className="text-sm text-gray-500">{workout.duration} minutes</p>
+              <p className="text-sm text-gray-500">{workout.steps} steps</p>
+              <p className="text-sm text-gray-500">{workout.activeTime} minutes active</p>
+              <p className="text-sm text-gray-500">{workout.caloriesBurnt} calories burnt</p>
+            </div>
+            <div className="p-4">
+              <HeartAnimation steps={workout.steps} activeTime={workout.activeTime} caloriesBurnt={workout.caloriesBurnt} />
+            </div>
           </div>
-        </div>
+        ))}
       </div>
+      <Link to="/workoutform" className="inline-block mt-4 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600">
+        Add Workout
+      </Link>
     </div>
   );
 };
