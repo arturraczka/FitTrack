01381197c31cf2f793e5bb9737076e531f2060e6
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-05-27 14:47:55 +0100

    Update text content in About component

---
+++ fit_tracker/frontend/src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-background">
+      <h1 className="text-4xl font-bold text-center text-text mt-10">About Us</h1>
+      <p className="text-lg text-center text-text mt-4">We are a team of fitness enthusiasts who want to help you stay fit and healthy.</p>
+      <p className="text-lg text-center text-text mt-4">Our mission is to provide you with the best tools and resources to achieve your fitness goals.</p>
+    </div>
+  );
+};
+
+export default About;
