1406f11f3e0bc79d82efe29bb34c4410304162e8
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-05-28 13:25:44 +0100

    Add margin-top of 1rem to Workout list items

--- fit_tracker/frontend/src/components/WorkoutList.jsx
+++ fit_tracker/frontend/src/components/WorkoutList.jsx
@@ -3,7 +3,7 @@ import HeartAnimation from './HeartAnimation';
 const WorkoutList = ({ workouts }) => {
   return (
     <div className="container mx-auto px-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
         {workouts.map((workout) => (
           <div key={workout.id} className="bg-white rounded-lg shadow-lg overflow-hidden">
             <div className="p-4">
