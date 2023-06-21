946b9c0a873b4d4da3c326b1af005519f1e6e877
Artur Raczka <art.raczka@gmail.com>
2023-06-21 22:40:03 +0200

    urlpatterns fix for api/urls config SIMPLE_JWT setting fixed for auth header type: Bearer

--- fit_tracker/apps/api/urls.py
+++ fit_tracker/apps/api/urls.py
@@ -7,7 +7,7 @@ session_router.register(r'session', SessionViewSet)
 
 urlpatterns = [
     path('', include(session_router.urls)),
-    path('summary', SummaryListView.as_view(), name = 'summary'),
+    path('summary/', SummaryListView.as_view(), name = 'summary'),
     path('summary/<str:summary_type>/', SummaryWithSessionListView.as_view(), name = 'summary-type'),
     path('user/', include('apps.user.urls')),
 ]
